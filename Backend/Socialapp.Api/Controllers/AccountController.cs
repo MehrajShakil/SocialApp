9d019273bcedb748100471e7b1feee47e44eb78a
MehrajShakil <sisakilcste18@gmail.com>
2023-05-27 19:52:55 +0600

    register and login added

--- Backend/Socialapp.Api/Controllers/AccountController.cs
+++ Backend/Socialapp.Api/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Socialapp.Api.Data;
+using Socialapp.Api.DTOs;
 using Socialapp.Api.Entities;
 using System.Security.Cryptography;
 using System.Text;
@@ -17,14 +19,19 @@ namespace Socialapp.Api.Controllers
         }
 
         [HttpPost("register")] // POST: api/account/register
-
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+
+            if(await UserExists(registerDto.UserName))
+            {
+                return BadRequest("UserName is taken");
+            }
+
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
 
@@ -32,6 +39,33 @@ namespace Socialapp.Api.Controllers
             await _Context.SaveChangesAsync();
 
             return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _Context.Users.SingleOrDefaultAsync(user => user.UserName == loginDto.UserName);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(var i = 0; i < computeHash.Length; i++)
+            {
+                if (computeHash[i] != user.PasswordHash[i])
+                {
+                    return Unauthorized("Invalid password");
+                }
+            }
+            return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _Context.Users.AnyAsync(user => user.UserName == username.ToLower());
         }
 
     }
---
+++ Backend/Socialapp.Api/DTOs/LoginDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Socialapp.Api.DTOs
+{
+    public class LoginDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
