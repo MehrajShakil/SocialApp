a8f30772ab9a7959cc4d87484f621069aa113a90
MehrajShakil <sisakilcste18@gmail.com>
2023-07-04 22:27:14 +0600

    reactive form initialize

--- Frontend/socialapp.client/src/app/register/register.component.ts
+++ Frontend/socialapp.client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_Services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,21 +11,34 @@ import { ToastrService } from 'ngx-toastr';
 export class RegisterComponent implements OnInit {
   model: any = {}
   @Output() cancelRegister = new EventEmitter();
+  registerForm!: FormGroup;
 
 
   constructor(private accountService: AccountService,
               private toast: ToastrService) { }
 
   ngOnInit() {
+    this.initializeForm();
+  }
+
+  initializeForm(){
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    })
   }
 
   register(){
-    this.accountService.register(this.model).subscribe({
-      next: () => {
-        this.cancle()
-      },
-      error: (error: any) => this.toast.error(error.error)
-    })
+
+    console.log(this.registerForm?.value);
+
+    // this.accountService.register(this.model).subscribe({
+    //   next: () => {
+    //     this.cancle()
+    //   },
+    //   error: (error: any) => this.toast.error(error.error)
+    // })
   }
 
   cancle(){
